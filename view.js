514d5e7016b53b187fce37841aeb25acfa12aba1
MariM-16 <69335919+MariM-16@users.noreply.github.com>
2021-05-24 05:33:00 -0400

    .

--- view.js
+++ view.js
@@ -1,18 +1,56 @@
+var prompt = require('prompt-sync')({sigint:true});  
 var figlet = require('figlet');
- 
-const chalk = require('chalk');
+const chalk = require('chalk');  
+const inquirer = require('inquirer')
 
-figlet.text('Unit Converter App', {
-    horizontalLayout: 'fitted',
-    verticalLayout: "default",
-    width: 80,
-    whitespaceBreak: false
-}, function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
+function get_title(){
+    return chalk.blue( 
+        figlet.textSync(
+            'Unit Converter App', 
+            {
+                horizontalLayout: 'full',
+                font: 'Nancyj-Underlined'
+            }
+        )
+    )
+};
+
+function ValueQ(varia){
+    return inquirer.prompt([
+      {
+        name: 'Option',
+        type: 'confirm',
+        message: 'Left temperature is source? ',
+        default: false,
+        
+      },
+      {
+        name: 'Temperature',
+        type: 'input',
+        message: 'Temperature value to convert? ',
+        default: "",
+      },
+      {
+        type: 'list',
+        name: 'Unit',
+        message: 'From? ',
+        choices: ['Celsius', 'Fahrenheit', 'Kelvin']
+      },
+      {
+        type: 'list',
+        name: 'Convert',
+        message: 'To? ',
+        choices: ['Celsius', 'Fahrenheit', 'Kelvin']
+      }
+    ])
+  }
+
+function view(){
+    return {
+      title: get_title()
     }
-    console.log( chalk.yellow(data));
-    
-});
+  }
+  module.exports = {
+    view,
+    ValueQ
+  }
