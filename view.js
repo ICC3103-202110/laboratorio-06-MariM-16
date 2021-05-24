1bf91722a053ae87c5e50d4a4fdd187078a0c252
MariM-16 <69335919+MariM-16@users.noreply.github.com>
2021-05-24 04:38:22 -0400

    t

--- view.js
+++ view.js
@@ -1,5 +1,18 @@
-figlet.defaults({fontPath: "assets/fonts"});
+var figlet = require('figlet');
+ 
+const chalk = require('chalk');
 
-function ready(){
-    console.log(figlet.textSync("Unit Converter App"));
-}
+figlet.text('Unit Converter App', {
+    horizontalLayout: 'fitted',
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: false
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log( chalk.yellow(data));
+    
+});
